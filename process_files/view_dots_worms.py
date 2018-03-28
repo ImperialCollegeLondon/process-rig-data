89fc296628eb7f9260b099ee3cb2f25680905686
Unknown <ver228@gmail.com>
2018-03-28 21:13:41 +0100

    .

--- process_files/view_dots_worms.py
+++ process_files/view_dots_worms.py
@@ -36,9 +36,11 @@ def get_n_worms_estimate(frame_numbers, percentile = 99):
 
 def plot_img_ch(exp_row, frame_number=0, resize_factor = 0.25):
     mask_dir = exp_row['directory']
-    results_dir = mask_dir.replace('MaskedVideos', 'Results')
-    mask_file = os.path.join(mask_dir, exp_row['base_name'] + exp_row['ext'])
-    skeletons_file = os.path.join(results_dir, exp_row['base_name'] + '_skeletons.hdf5')
+    mask_file = os.path.join(mask_dir, exp_row['base_name']) #exp_row['ext'])
+    if not mask_file.endswith('.hdf5'):
+        mask_file += '.hdf5'
+    
+    skeletons_file = mask_file.replace('MaskedVideos', 'Results').replace('.hdf5', '_skeletons.hdf5')
     
     with tables.File(mask_file, 'r') as fid:
         img = fid.get_node('/full_data')[0]
@@ -55,7 +57,6 @@ def plot_img_ch(exp_row, frame_number=0, resize_factor = 0.25):
     
     with pd.HDFStore(skeletons_file, 'r') as fid:
         trajectories_data = fid['/trajectories_data']
-    
     
     trajectories_data = trajectories_data[trajectories_data['is_good_skel'] == 1]
     n_worms_estimate = get_n_worms_estimate(trajectories_data['frame_number'])
@@ -114,27 +115,29 @@ def check_one_ch_per_set(exp_data):
         for set_del_t, delta_data in set_data.groupby('set_delta_time'):
             for stage_pos, pos_data in delta_data.groupby('stage_pos'):
                 if len(pos_data.channel.unique()) != len(pos_data.channel):
+                    
+                    import pdb
+                    pdb.set_trace()
                     msg = 'Set{} Pos{} T{} has more than one video per channel'.format(set_n, stage_pos, set_del_t) 
                     raise(ValueError(msg))
                 
                 
     return good
 
-def make_plate_views(root_dir, exp_name, max_del_t = 2):
+def make_plate_views(root_dir, exp_name, exp_data = None, max_del_t = 2, ignore_err=False):
     '''
     max_del_t -> max delta time in minutes consider movies as the same temporal set.
     '''
 
-    csv_file = _get_csv_fname(os.path.join(root_dir, 'ExtraFiles', exp_name))
-    search_str = os.path.join(root_dir, 'MaskedVideos', exp_name, '*.hdf5')
-    mask_files = glob.glob(search_str)
+    if exp_data is None:
+        csv_file = _get_csv_fname(os.path.join(root_dir, 'ExtraFiles', exp_name))
+        search_str = os.path.join(root_dir, 'MaskedVideos', exp_name, '*.hdf5')
+        mask_files = glob.glob(search_str)
+        exp_data = get_rig_experiments_df(mask_files, [csv_file])
     
-    exp_data = get_rig_experiments_df(mask_files, [csv_file])
-    
-    exp_data['set_delta_time'] = np.floor(exp_data['set_delta_time']/max_del_t)*max_del_t
-    check_one_ch_per_set(exp_data)
-    
-    
+    if 'set_delta_time' in exp_data:
+        exp_data['set_delta_time'] = np.floor(exp_data['set_delta_time']/max_del_t)*max_del_t
+        check_one_ch_per_set(exp_data)
     
     exp_data['n_worms_estimate'] = np.nan
     figs2save = []
@@ -146,7 +149,14 @@ def make_plate_views(root_dir, exp_name, max_del_t = 2):
                 title_str = 'Set{} Pos{} T{}min'.format(set_n, stage_pos, set_del_t)
                 
                 for irow, row in pos_data.iterrows():
-                    n_worms_estimate = plot_img_ch(row)
+                    try:
+                        n_worms_estimate = plot_img_ch(row)
+                    except:
+                        if ignore_err:
+                            continue
+                        else:
+                            raise
+                    
                     exp_data.loc[irow, 'n_worms_estimate'] = n_worms_estimate
                     
                 plt.subplots_adjust(wspace=0.01, hspace=0.01)
@@ -185,17 +195,43 @@ def make_plate_views(root_dir, exp_name, max_del_t = 2):
         
     pdf_pages.close()
 
+def _plate_views_with_csv(dname):
+    exp_names = [x for x in os.listdir(dname) if os.path.isdir(os.path.join(dname,x))]
+    for exp_name in exp_names:
+        make_plate_views(root_dir, exp_name, max_del_t=2)
 
+#%%
 if __name__ == '__main__':
     #root_dir = '/Volumes/behavgenom_archive$/Avelino/PeterAskjaer/'
     #exp_name = 'Mutant_worm_screening_Y32H12A.7(ok3452)_220217'
     #root_dir = '/Volumes/behavgenom_archive$/Avelino/Worm_Rig_Tests/movies_2h/'
     #root_dir = '/Volumes/behavgenom_archive$/Avelino/Worm_Rig_Tests/short_movies_new/'
-    root_dir = '/Volumes/behavgenom_archive$/Avelino/screening/CeNDR/'
+    #root_dir = '/Volumes/behavgenom_archive$/Avelino/screening/CeNDR/'
+    root_dir = '/Volumes/behavgenom_archive$/Serena/AggregationScreening/'
     #root_dir = '/Volumes/behavgenom_archive$/Adam/screening/antipsychotics'
     dname = os.path.join(root_dir, 'MaskedVideos')
-    exp_names = [x for x in os.listdir(dname) if os.path.isdir(os.path.join(dname,x))]
-    for exp_name in exp_names:
-        make_plate_views(root_dir, exp_name, max_del_t=2)
+    
+    data_file = os.path.join(root_dir, 'aggregation_data_edit.csv')
+    df_info = pd.read_csv(data_file)
+    df_info = df_info.rename(columns={'basename':'base_name', 
+                                      'dirname':'directory',
+                                      'run':'set_n',
+                                      'Dot':'mark'}, copy=False)
+    df_info['set_delta_time'] = 0
+    df_info['stage_pos'] = 0
+    df_info['n_worms'] = 0
+    
+    #remove invalid data
+    df_info = df_info[df_info['block']>0]
+    df_info = df_info[~df_info['base_name'].str.lower().str.contains('_x_')]
+    #%%
+    for (block, day), dat in df_info.groupby(('block', 'day')):
+        exp_name = '{}.{}'.format(block, day)
+        print(exp_name)
+        make_plate_views(root_dir, exp_name, exp_data = dat, ignore_err=True)
+    
+    
+    #_plate_views_with_csv(dname)
+    
         
     
